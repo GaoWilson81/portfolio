95fde9361c99ddf459db5627555b19f16656d7b1
GaoWilson81 <wilsongao2002@gmail.com>
2024-06-26 13:15:53 -0400

    update card
    entered second project description

--- components/Primary.tsx
+++ components/Primary.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import gif from '../files/epic.gif';
 import Image from 'next/image';
 //Card component
-const Card = () => {
+const Card1 = () => {
   return (
     <div className='d-flex justify-content-center align-items-center'>
       <div className="card">
@@ -22,6 +22,28 @@ const Card = () => {
     </div>
   );
 };
+const Card2 = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center'>
+      <div className="card">
+        <div className="card-body">
+          <Image className='w-full h-full' src={gif} alt='Epic Dashboard'/>
+          <h5 className="card-title">Package Delivery Management</h5>
+          <p className="card-text">This project included creating an Enhanced Entity Relationship Diagram (EERD),
+             converting it into a relational schema, 
+             and writing table statements for database management. 
+             Emphasis was placed on thorough documentation and organizing data for various entity types. 
+             This experience helped me apply database design principles to solve practical problems effectively.
+          </p>
+            <p>
+              <b>Built with:</b>
+              <p> SQL, MySQL</p>
+            </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // Primary layout
 const Primary = () => {
@@ -30,10 +52,10 @@ const Primary = () => {
       <div className="container">
         <div className="row">
           <div className="pb-20 col-md-60">
-            <Card />
+            <Card1 />
           </div>
           <div className="pb-20 col-md-6">
-            <Card />
+            <Card2 />
           </div>
         </div>
       </div>
